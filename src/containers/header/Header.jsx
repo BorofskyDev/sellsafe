0f750728bf9ea2b3b2bbb5edd8bca70a335a1d8d
solovyovsdom <joelborofsky@mac.com>
2022-01-31 21:45:33 -0600

    completed header section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './Header.css'
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className='gpt3__header section__padding' id='home'>
+            <div className='gpt3__header-content'>
+                <h1 className='gradient__text'>Let's Build Something amazing with GPT3-OpenAI</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam sem et tortor consequat id porta nibh venenatis cras. A condimentum vitae sapien pellentesque habitant morbi tristique.</p>
+                <div className='gpt3__header-content__input'>
+                    <input type='email' placeholder='Your Email' />
+                    <button type='button'>Get Started</button>
+                </div>
+                <div className='gpt3__header-content__people'>
+                    <img src={people} alt='people' />
+                    <p>1,600 people requested access</p>
+                </div>
+            </div>
+            <div className='gpt3__header-image'>
+                <img src={ai} alt='ai' />
+            </div>
         </div>
     );
 };
