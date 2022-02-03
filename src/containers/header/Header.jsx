babbc6ee4ad6d6244d74f853f3df1732b3000946
solovyovsdom <joelborofsky@mac.com>
2022-02-03 00:11:31 -0600

    added button styling, removed input from header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.css'
 import people from '../../assets/people.png'
 import ai from '../../assets/ai.png'
+import { FaEnvelope, FaFacebook, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa'
 
 const Header = () => {
     return (
@@ -10,12 +11,16 @@ const Header = () => {
                 <h1 className='gradient__text'>Increase Your Cash Flow with SellSafe</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam sem et tortor consequat id porta nibh venenatis cras. A condimentum vitae sapien pellentesque habitant morbi tristique.</p>
                 <div className='SellSafe__header-content__input'>
-                    <input type='email' placeholder='Your Email' />
                     <button type='button'>Get Started</button>
                 </div>
                 <div className='SellSafe__header-content__people'>
-                    <img src={people} alt='people' />
-                    <p>1,600 people requested access</p>
+                    <button className='SellSafe__header-content__social'><FaFacebook className='SellSafe__header-content___social-icon'/></button>
+                    <button className='SellSafe__header-content__social'><FaLinkedin className='SellSafe__header-content___social-icon'/></button>
+                    <button className='SellSafe__header-content__social'><FaTwitter className='SellSafe__header-content___social-icon'/></button>
+                    <button className='SellSafe__header-content__social'><FaYoutube className='SellSafe__header-content___social-icon'/></button>
+                    <button className='SellSafe__header-content__social'><FaEnvelope className='SellSafe__header-content___social-icon'/></button>
+                    {/* <img src={people} alt='people' />
+                    <p>1,600 people requested access</p> */}
                 </div>
             </div>
             <div className='SellSafe__header-image'>
